c97357d10918340c63f5b8c93e80d1420d91fc03
Xolmuratov02 <xolmuratovqurbonali@gmail.com>
2023-10-17 14:15:46 +0500

    N51_HT

--- N52_HT/Program.cs
+++ N52_HT/Program.cs
@@ -9,24 +9,3 @@
 //- bu handler Email Sender service dan foydalanib, userga welcome email jo'natsin
 
 
-//Endpointlar yaratish:
-//Controllers ichida Orderlar va Productlar uchun endpointlar (misol: OrderController va
-//ProductController) yaratishimiz kerak. Bu endpointlar qaysi URL'lar orqali chaqirilishi kerakligini
-//aniqlash uchun dekoretorlar bilan belgilanadi.
-
-//Konfiguratsiya yaratish:
-//Startup.cs faylida ConfigureServices va Configure metodlarini ishlatib, konfiguratsiyani sozlashimiz
-//mumkin. Bu yerda siz Entity Framework, JWT avtorizatsiyasi, CORS, va boshqa konfiguratsiya sozlamalarni
-//sozlay olamiz.
-
-//Services va Models:
-//Services layerida Orderlar va Productlar uchun xizmatlarni yaratishimiz va bularni controllerlar
-//orqali chaqirishimiz kerak. Models layerida Orderlar va Productlar uchun klasslarni yaratishimiz kerak.
-
-//Dependency Injection (DI):
-//Controllerlar ichida ishlatiladigan xizmatlarni injekt qilishimiz kerak. Bu, services layeridagi
-//xizmatlarni controllerlarda ishlatish imkoniyatini beradi.
-
-//Foundation services:
-//Ha, biz controllers ichida foundation services yaratib ishlatishimiz mumkin. Bularni services
-//layerida yaratib, bu xizmatlarni controllerlarda yoki boshqa joylarda ishlatishimiz mumkin.
