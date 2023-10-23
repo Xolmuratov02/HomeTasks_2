e20b18ac56b10dd44a6f7f127f13bd8019d8feb3
Xolmuratov02 <xolmuratovqurbonali@gmail.com>
2023-10-23 13:55:29 +0500

    N55_HT

---
+++ N55_HT/FileFolderService.cs
@@ -0,0 +1,17 @@
+﻿namespace N55_HT;
+
+public class FileFolderService
+{
+    public static (int EntiresCount, int FoldersCount, int FilesCount) GetAmountOfEntries()
+    {
+        return (Directory.EnumerateFileSystemEntries(Directory.GetCurrentDirectory()).Count(),
+            Directory.GetDirectories(Directory.GetCurrentDirectory()).Count(),
+            Directory.GetFiles(Directory.GetCurrentDirectory()).Count());
+    }
+
+    public static (long AllSize, long MaxSize) GetSizeOfFiles()
+    {
+        var files = Directory.GetFiles(Directory.GetCurrentDirectory()).AsQueryable();
+        return (files.Sum(f => new FileInfo(f).Length), files.Max(f => new FileInfo(f).Length));
+    }
+}
---
+++ N55_HT/Program.cs
@@ -0,0 +1,15 @@
+﻿//N55-HT1
+
+//- Console application yarating
+//- asosiy project build papkasi ichiga ( bin/Debug/.net7 ) bir nechta folder va filelar copy qiling
+//- asosiy project papkasi ichida nechta folder va fayl borligini
+//- fayllar umumiy hajmi ( size ) va eng katta fayl qaysi ekanligini qaytaradigan service va methodlar
+// yozing ( nomlarini o'zingiz o'ylab ko'ring ) 
+//- tepada aytilgan ma'lumotlarni ekranga chiqaring
+
+using N55_HT;
+
+var Amount = FileFolderService.GetAmountOfEntries();
+var size = FileFolderService.GetSizeOfFiles();
+Console.WriteLine($"Count of\nEntries - {Amount.EntiresCount}\nFolders - {Amount.FoldersCount}\nFiles - {Amount.FilesCount}");
+Console.WriteLine($"\nAll size of files - {size.AllSize}\nMax size of files - {size.MaxSize}");
