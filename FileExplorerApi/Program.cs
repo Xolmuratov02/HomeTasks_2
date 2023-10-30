99130c2d088bb6015e33f49db292d79edccc0ba8
Xolmuratov02 <xolmuratovqurbonali@gmail.com>
2023-10-30 13:41:31 +0500

    Push

--- FileExplorerApi/Program.cs
+++ FileExplorerApi/Program.cs
@@ -1,11 +1,3 @@
-//N58-HT1
-
-//- file explorer uchun API qiling
-//- bunda Directory va File dan foydalanish uchun Broker lardan foydalaning
-//- sizda bitta endpoint bo'ladi - api/directories/root/entries
-//- bu endpoint wwwroot ( static fayllar turadigan joy ) dagi directorylar va fayllar haqida ma'lumot qaytaradi
-//- directory va faylni serialize qilib bo'lmaydi va shuning uchun bunga boshqa modellardan foydalanasiz, masalan StorageDirectory va StorageFile
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
