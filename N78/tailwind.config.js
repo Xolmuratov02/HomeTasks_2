b36e2349937dbbd5f2b27e239bff0e59fc209a70
Xolmuratov02 <xolmuratovqurbonali@gmail.com>
2023-12-05 21:35:46 +0500

    HTML

--- N78/tailwind.config.js
+++ N78/tailwind.config.js
@@ -4,13 +4,22 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '545px',
+      md: '745px',
+      lg: '950px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
         textPrimary: '#222222',
         textSecondary: '#818181',
         logoSelected: '#000000',
         logoSecondary: '#717171',
-        logoPrimary: '#ff385c'
+        logoAccent: '#dddddd',
+        logoPrimary: '#ff385c',
+        borderSecondary: '#cccccc',
+        defaultBackground: '#ffffff'
       }
     },
   },
